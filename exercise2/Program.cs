30a78b76aad002078ec3be8832b067e574d557d9
IvanKovalev <deformgame@gmail.com>
2022-08-21 10:30:51 +0300

    Saved Program.cs

---
+++ exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int numberX = 0;
+
+if (number1 > number2)
+{
+    numberX = number1;
+}
+else {
+    numberX = number2;
+}
+
+if (number3 > numberX)
+{
+   Console.WriteLine($"Число {number3} большее из трех");
+}
+else {
+    Console.WriteLine($"Число {numberX} большее из трех");
+}
