9f7cd93e1bdf23b2b171bd4237c5ae35a62367f1
IvanKovalev <deformgame@gmail.com>
2022-08-21 11:19:16 +0300

    Added the solution of the fourth task

---
+++ exercise4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+for (int N = 2; N <= number; N++)
+
+if (N % 2 == 0)
+{
+    Console.Write(N + ", ");
+}
