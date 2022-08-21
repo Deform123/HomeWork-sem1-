7cbc414cec480e551de278637f2099b44939a25e
IvanKovalev <deformgame@gmail.com>
2022-08-21 10:13:27 +0300

    Added a solution to the first task

---
+++ exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+   Console.WriteLine($"Число {number1} больше чем число{number2}"); 
+}
+
+else {
+   Console.WriteLine($"Число {number2} больше чем число{number1}");
+}
